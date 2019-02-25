dcf6ca7e947ed31730ea29511d5770cc2283c1c5
jaymile <djemil.aminou@gmail.com>
2019-02-26 00:34:57 +0100

    6 ok

--- script.js
+++ script.js
@@ -28,11 +28,55 @@ function greenColor() {
     texte2.classList.toggle("text-success")
 }
 
+// faire disparaitre la bootstrap 
+let linkRel = document.getElementsByTagName('link')[0];
+let navBarHeader = document.getElementsByClassName('navbar navbar-dark bg-dark box-shadow')[0];
+let nuclearStatus = false 
+function nuclearToggle() {
+  if(nuclearStatus == false) {
+    console.log('wtf');
+    linkRel.remove();
+    nuclearStatus = true;
+  } else if (nuclearStatus == true) {
+    var head = document.head;
+    var link = document.createElement("link");
+  
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
+  
+    head.appendChild(link);
+  }
+}
 
+navBarHeader.ondblclick = nuclearToggle;
 
+//fonction 6 hover de chez hover
 
+let viewBtn = document.querySelectorAll('.btn-success');
+let viewBtnStatus = false;
 
+function viewToggle(j) {
+  let textCard = document.querySelectorAll('.card-text');
+  let imageCard = document.querySelectorAll('.card-img-top');
 
+  if(viewBtnStatus == false) {
+    textCard[j].style.visibility = 'hidden';
+    imageCard[j].style.width = '20%';
+    imageCard[j].style.height = 'auto';
+    viewBtnStatus = true;
+  } else if(viewBtnStatus == true) {
+    console.log('ok');
+    textCard[j].style.visibility = 'visible';
+    imageCard[j].style.width = '100%';
+    imageCard[j].style.height = 'auto';
+    viewBtnStatus = false;
+  };
+};
 
-    
-    
+for (let j = 0; j < viewBtn.length; j++) {
+  let button = viewBtn[j];
+  button.addEventListener('mouseover', function() {
+    viewToggle(j);
+  });
+}
